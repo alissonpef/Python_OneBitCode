e654102db750d7bee5af9ca558883a5bc1180a90
Alisson Pereira <alissonpef@gmail.com>
2025-01-07 16:00:28 -0300

    First commit

---
+++ Introduction_To_Python/Exercises/1-Antecessor_e_Sucessor.py
@@ -0,0 +1,10 @@
+"""
+Exercicio 1:
+* Antecessor e Sucessor de um número:
+-> Escreva um programa em Python que leia um número e represente o número antecessor e sucessor desse número que foi 
+lido, utilizando operadores de atribuição
+"""
+
+number = int(input("Digite um número: "))
+
+print(f"O antecessor do {number} é o número {number - 1} e o sucessor é o número {number + 1}")
