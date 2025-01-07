97a14e1925161965fe36ed629b853235171d2ede
Álisson Pereira Ferreira <113950309+alissonpef@users.noreply.github.com>
2025-01-07 15:56:34 -0300

    Delete Módulos_e_PIP directory

--- Módulos_e_PIP/1-Modulos.py
+++
@@ -1,7 +0,0 @@
-import Calc
-from Calc import div 
-
-print(Calc.sum(10, 2))
-print(Calc.sub(10, 2))
-print(Calc.mult(10, 2))
-print(div(10, 2))
--- Módulos_e_PIP/Exercicios/Strings.py
+++
@@ -1,9 +0,0 @@
-def inverte(a):
-    return a[::-1] ## Inicio:Fim:Passo
-
-def par(a):
-    return a[0::2] ## Inicio:Fim:Passo
-
-def impar(a):
-    return a[1::2] ## Inicio:Fim:Passo
-
