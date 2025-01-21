9c6c814d0d80d11167081c908b457c52140d13d1
Alisson Pereira <alissonpef@gmail.com>
2025-01-21 17:26:14 -0300

    New commit

---
+++ 3-POO/1-Classes.py
@@ -0,0 +1,6 @@
+class Movie: # Classe geralmente começa com a letra maíscula
+    name = ""
+    yearLaunch = 0
+    includedPlan = False
+    note = 0
+    durationMinutes = 0
---
+++ 3-POO/3-Construtor.py
@@ -0,0 +1,15 @@
+class Movie:
+    # Com o Construtor, podemos criar o bojeto direto na chamada dele, sem ter que fazer Classe.atributo = ...
+    def __init__(self, name, yearLaunch, includedPlan, note, durationMinutes):
+        self.name = name
+        self.yearLaunch = yearLaunch
+        self.includedPlan = includedPlan
+        self.note = note
+        self.durationMinutes = durationMinutes
+    # Dessa maneira, ao charmarmos print(movie) ele retorna o nome do filme, ao em vez de: <__main__.Movie object at 0x000001D7EBFF6A50>
+    def __str__(self):
+        return f"Filme: {self.name}"
+
+movie = Movie("Super Mario", 2023, False, 10.0, 120)
+print(movie)
+print(movie.name)
